1b088eba570e7f9c5927493167c4b89416ce779b
Brian Icochea <Ibrian93@gmail.com>
2021-10-30 17:16:47 +0200

    investigating base URL on config

--- DemoQA/src/config/config.ts
+++ DemoQA/src/config/config.ts
@@ -1,8 +1,19 @@
-const config = {
-  baseUrl: 'https://demoqa.com/books',
-  browser: 'chromium',
-  runHeadless: true,
-  runSlow: 0,
+import { PlaywrightTestConfig } from "@playwright/test"
+
+const config : PlaywrightTestConfig= {
+  timeout: 30000,
+  testDir: '${pwd}/../../../tests/',
+  projects: [
+    {
+      name: 'Chrome',
+      use: {
+        baseURL: 'https://demoqa.com/books',
+        browserName: 'chromium',
+        headless: false,
+        actionTimeout: 0,
+      }
+    }
+  ]
 }
 
 export default config
--- DemoQA/tests/login.test.ts
+++ DemoQA/tests/login.test.ts
@@ -1,8 +1,12 @@
-import { expect } from '@playwright/test';
+import { HomePage } from '../src/pages/home.page';
 import test  from './user.fixture';
 
 
 test.describe('User login in the DemoQA website', async () => {
-    test('User login with correct credentials', async({page, user}) => {
+    test('User login with correct credentials', async({page, context}) => {
+        const homepage = new HomePage(page, context);
+        await page.goto('/test')
+        //await homepage.goToHomePage();
+        //await homepage.goToLoginPage();
     })
 });
