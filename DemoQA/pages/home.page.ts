a028ad18d764f9411df8aaf45fc23fc218e92a3b
Brian Icochea <Ibrian93@gmail.com>
2021-11-28 13:25:23 +0100

    fixing lint

--- DemoQA/pages/home.page.ts
+++ DemoQA/pages/home.page.ts
@@ -2,7 +2,6 @@ import { Page } from '@playwright/test';
 
 export class HomePage {
   public get elements() {
-
     return {
       loginButton: 'text=Login',
       userNameValue: 'id=userName-value',
